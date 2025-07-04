65ecebd8ec8afad44892165af42c706b2bbb193b
Seulgi Lee <lsg4250@gmail.com>
2025-07-04 19:04:50 +0900

    Title: 문자 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250704/문자 출력하기/print-text.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int i;
+    char a;
+    scanf("%c", &a);
+    for (i = 0; i < 8; i++){
+        printf("%c", a);
+    }
+    return 0;
+}
