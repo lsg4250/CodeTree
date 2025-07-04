7fafd322bbe1bd9232939717ab1650b2e3ae6ef9
Seulgi Lee <lsg4250@gmail.com>
2025-07-04 19:11:49 +0900

    Title: N번 더하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250704/N번 더하기/add-n-times.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a, n;
+    scanf("%d %d", &a, &n);
+    for (int i = 0; i < n; i++){
+        a += n;
+        printf("%d", a);
+    }
+    return 0;
+}
